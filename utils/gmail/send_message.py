5512cf8c9aa69a5d1f51fc2d68e8d195c058a155
Jose Fernando Álvarez Romero <joseferalvarezromero@gmail.com>
2024-07-31 20:02:07 +0200

    amazon ebook limit fixed

--- utils/gmail/send_message.py
+++ utils/gmail/send_message.py
@@ -15,28 +15,29 @@ def send_message(service, attachments, sender, recipient):
     mime_message["Subject"] = f"Newsletters of the day {date.today()}"
     mime_message.set_content("Newsletter attachments:")
 
+    attachments_number = 0
+    next_attachments = []
+
     for attachment in attachments:
 
-      filename = os.path.basename(attachment)
+      if attachments_number < 20:
+        filename = os.path.basename(attachment)
 
-      with open(attachment, "rb") as file:
-        data = file.read()
+        with open(attachment, "rb") as file:
+          data = file.read()
 
-      mime_message.add_attachment(data, maintype="application", subtype="epub+zip", filename=filename)
-  
+        mime_message.add_attachment(data, maintype="application", subtype="epub+zip", filename=filename)
+        attachments_number += 1
+      else:
+        next_attachments.append(attachment)
+
     encoded_message = base64.urlsafe_b64encode(mime_message.as_bytes()).decode()
     message = {"raw": encoded_message}
   
-    send_message = (
-        service.users()
-        .messages()
-        .send(userId="me", body=message)
-        .execute()
-    )
+    service.users().messages().send(userId="me", body=message).execute()
 
-    return send_message
+    if(len(next_attachments) > 1):
+      send_message(service=service, attachments=next_attachments, sender=sender, recipient=recipient)
+
   except HttpError as error:
     print(error)
-    send_message = None
-
-  return send_message
